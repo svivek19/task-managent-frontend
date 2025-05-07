a6fea29cb0927218de4b671a26990344a7d697ee
svivek19 <svivek030503@gmail.com>
2025-05-07 16:35:49 +0530

    feat: update Manage Task component with styled layout and placeholder content

--- src/pages/ManageTask.jsx
+++ src/pages/ManageTask.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ManageTask = () => {
-  return <div>ManageTask</div>;
+  return (
+    <div className="bg-white p-4 rounded-lg shadow-md">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, repellat
+      pariatur sed perferendis assumenda suscipit vero ea quisquam unde dolores
+      laborum commodi ipsa maiores numquam illo laboriosam, distinctio molestiae
+      sint.
+    </div>
+  );
 };
 
 export default ManageTask;
