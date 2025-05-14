6f71909fd41954d081ac30193acf2a22c8710298
svivek19 <svivek030503@gmail.com>
2025-05-14 10:17:31 +0530

    feat: add PWA support with vite-plugin-pwa and include app icon

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,43 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: [
+        "favicon.svg",
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+      ],
+      manifest: {
+        name: "TaskFlow",
+        short_name: "TaskFlow",
+        description:
+          "Stay organized and productive. Log in to access your personalized task dashboard, manage your to-dos, and track your progress.",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "./src/assets/pwa-icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./src/assets/pwa-icon.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
 });
